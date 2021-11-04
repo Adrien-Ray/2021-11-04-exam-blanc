a095432fad1ba05884d91e8c4ce926430a580f49
Adrien-Ray <a.raymond@codeur.online>
2021-11-04 11:47:04 +0100

    finish

---
+++ integration/index.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+      <meta charset="UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>examen blanc</title>
+      <link rel="shortcut icon" href="assets/img/logo.svg" type="image/x-icon">
+      <link rel="stylesheet" href="assets/style/reset.css">
+      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+      <link rel="stylesheet" href="assets/style/style.css">
+</head>
+<body>
+      <nav class="sticky-top navbar navbar-expand-lg navbar-light bg-light">
+            <div class="container-fluid">
+                  <a class="navbar-brand" href="#"><img class="logonavbar" src="assets/img/logo.svg" alt="logo of website"></a>
+                  <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                  </button>
+                  <div class="nav__div collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul class="nav__div__ul navbar-nav me-auto mb-2 mb-lg-0">
+                              <li class="nav-item">
+                                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                              </li>
+                              <li class="nav-item">
+                                    <a class="nav-link active" aria-current="page" href="#product">Product</a>
+                              </li>
+                              <li class="nav-item">
+                                    <a class="nav-link active" aria-current="page" href="#works">How it works.</a>
+                              </li>
+                        </ul>
+                  </div>
+            </div>
+      </nav>
+      <section>
+            <h2 class="section__child">Beautiful food & takeaway, <span class="h2__color">delivered</span> to your door.</h2>
+            <div class="section__flex">
+                  <img src="assets/img/hero_image.png" alt="image hero">
+                  <p class="section__child">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500. <br><button class="section__button">Place an order</button></p>
+            </div>
+      </section>
+      <section id="product">
+            <h2 class="section__child"><span class="h2__color">The home of fresh products</span></h2>
+            <div class="section__flex">
+                  <img src="assets/img/eat.png" alt="person eat">
+                  <p class="section__child">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500. <br><button class="section__button">Learn about us</button></p>
+            </div>
+      </section>
+      <section id="works">
+            <h2 class="section__child"><span class="h2__color">How it works.</span></h2>
+            <div class="section__cards__parent">
+                  <div class="card" style="width: 18rem;">
+                        <img src="assets/img/step_01.png" class="card-img-top" alt="step_01">
+                        <div class="card-body">
+                              <p class="card-text"><span class="card_title">Adapt your menu items</span><br><br>Easily adapt your menu using the webflow CMS and allow customers to browse your items.</p>
+                        </div>
+                  </div>
+                  <div class="card" style="width: 18rem;">
+                        <img src="assets/img/step_02.png" class="card-img-top" alt="step_02">
+                        <div class="card-body">
+                              <p class="card-text"><span class="card_title">Accept online orders & takeout</span><br><br>Let your customers order and pay via the powerful ecommerce system, or simple let them call your store.</p>
+                        </div>
+                  </div>
+                  <div class="card" style="width: 18rem;">
+                        <img src="assets/img/step_03.png" class="card-img-top" alt="step_03">
+                        <div class="card-body">
+                              <p class="card-text"><span class="card_title">Manage delivery or takeout</span><br><br>Manage your own logistics and take orders simply through the ecommerce system.</p>
+                        </div>
+                  </div>
+            </div>
+      </section>
+      <footer>
+            <p>
+                  Powered by FoodApp
+            </p>
+            <p>
+                  <a href="">CGV</a>  <a href="">About us</a>
+            </p>
+      </footer>
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+      <script src="assets/script/main.js"></script>
+</body>
+</html>
