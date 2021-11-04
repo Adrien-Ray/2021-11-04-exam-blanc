4513736fa45f35e4aa4c1b670dc3b443d503a346
Adrien-Ray <forpub@accesdenied.net>
2021-11-04 16:23:03 +0100

    burger

--- integration-correction/assets/scripts/navbar.js
+++ integration-correction/assets/scripts/navbar.js
@@ -1,3 +1,3 @@
 document.getElementById('header__nav--burger').addEventListener('click', function () {
-      document.getElementsByClassName('header__nav--list')[0].classList.toggle('showNav');
+      document.getElementsByClassName('header__nav')[0].classList.toggle('showNav');
 })
