84cb5eea4a1f8ab20e7bb33124ca0975e465f837
Adrien-Ray <a.raymond@codeur.online>
2021-11-04 15:26:27 +0100

    correction

---
+++ integration-correction/assets/scripts/navbar.js
@@ -0,0 +1,3 @@
+document.getElementById('header__nav--burger').addEventListener('click', function () {
+      document.getElementsByClassName('header__nav--list')[0].classList.toggle('showNav');
+})
